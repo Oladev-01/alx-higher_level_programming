aafbe1d9a0a09ba9ee3666b04020f694086ba18e
olalekan <lekanmojibola@gmail.com>
2023-12-01 06:57:34 +0100

    This is a python code

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - this function checks for the occurrence of cycled linked lists
+ * @list: this is the list to be parsed,
+ * i.e the head to the first node of the list
+ * Return: 1 for cycle linked list and 0 for linear linked list
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	current = list;
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == list)
+			return (1);
+	}
+	return (0);
+}
