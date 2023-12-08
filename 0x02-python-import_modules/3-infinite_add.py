4795f2a3f7aed1abb5b675f235e393c36ada7b87
olalekan <lekanmojibola@gmail.com>
2023-12-08 19:40:36 +0100

    This is a python code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 a = 0
-for i in range(1, len(argv)):
-    a += int(argv[i])
+if __name__ == "__main__":
+    for i in range(1, len(argv)):
+        a += int(argv[i])
 
 print("{}".format(a))
