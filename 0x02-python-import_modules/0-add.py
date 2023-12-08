66475e49303c7446bbdaaa9e46e7e0918d405cb6
olalekan <lekanmojibola@gmail.com>
2023-12-08 16:37:06 +0100

    This is a python code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,8 @@
 a = 1
 b = 2
 if __name__ == "__main__":
-    from add_0 import add
+    from add_0 import add as olalekan
 
-    result = add(a, b)
+    result = olalekan(a, b)
     print("{} + {} = {}".format(a, b, result))
+ 
