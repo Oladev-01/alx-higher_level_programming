47fcaa173338c46f7693cc165b79709e9f8d1d73
olalekan <lekanmojibola@gmail.com>
2023-12-12 14:54:38 +0100

    This is a python code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list is None or not my_list:
         return None
     else:
-        a = 0
+        a = my_list[0]
         for i in my_list:
             if i > a:
                 a = i
