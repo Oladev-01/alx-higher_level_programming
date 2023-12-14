5b57c57abc1c32136323a182fd359b2abe980ff2
olalekan <lekanmojibola@gmail.com>
2023-12-14 17:03:40 +0100

    This is a python code

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - this function uses the cpython API
+ *  to print some basic info about python list
+ * @p: this is the pointer to the python list object
+ * Return: void
+*/
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	PyObject *obj;
+
+	size = PyList_Size(p);
+	alloc = ((PyObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
+	}
+}
