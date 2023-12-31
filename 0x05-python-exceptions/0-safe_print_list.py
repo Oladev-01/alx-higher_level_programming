93727158d4e0a6452b15a9a5c479d68e1f06b20c
olalekan <lekanmojibola@gmail.com>
2023-12-31 08:31:47 +0100

    This is a python code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     if not my_list or my_list is None:
-        return None
+        return 0
     size = 0
     try:
         for i in range(x):
