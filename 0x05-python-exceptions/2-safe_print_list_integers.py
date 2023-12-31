649810c323e4261572b238c71ac850409415a162
olalekan <lekanmojibola@gmail.com>
2023-12-31 10:01:15 +0100

    This is a python code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    size = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                size += 1
+    except TypeError:
+        pass
+    print()
+    return size
