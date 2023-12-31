aae5ee7039cfa30fd0c015099815c6a5ecb56c16
olalekan <lekanmojibola@gmail.com>
2023-12-31 10:21:09 +0100

    This is a python code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        if not a or not b:
+            raise ValueError
+        elif b == 0:
+            raise ZeroDivisionError
+        else:
+            c = a / b
+    except (ValueError, ZeroDivisionError):
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+        return c
