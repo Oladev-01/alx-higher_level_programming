56069d1c4d194e728d6006043ab736490f3e5410
olalekan <lekanmojibola@gmail.com>
2024-01-02 12:48:12 +0100

    This is a python code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 4):
+    for i in range(2, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += (a ** b) / i
+            result += (a ** b) // i
         except Exception:
             result += a + b
     return result
