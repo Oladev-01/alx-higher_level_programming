598c8e9911ae81068582199541406da8879dc10f
olalekan <lekanmojibola@gmail.com>
2024-01-02 11:00:11 +0100

    This is a python code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}\n".format(value))
+        print("{:d}".format(value))
         return True
     except ValueError:
         sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
