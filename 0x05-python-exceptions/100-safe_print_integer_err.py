cbf6bc0e2de6e9aec5d2d02185eba91e7cb04492
olalekan <lekanmojibola@gmail.com>
2024-01-02 10:23:03 +0100

    This is a python code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        if not isinstance(value, int):
+            raise ValueError
+        else:
+            print("{:d}".format(value))
+    except ValueError:
+        sys.stderr.write(("Exception: Unknown format code 'd' "
+                          "for object of type 'str'\n"))
+        return False
+    return True
