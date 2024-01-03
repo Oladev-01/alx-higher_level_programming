9f8dfb7d48851b6e636a7a8d8e7ce1d5d9b4e7df
olalekan <lekanmojibola@gmail.com>
2024-01-03 13:03:02 +0100

    This is a python code

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module imports an empty class that defines a square """
+
+
+class Square:
+    """ This is an empty class that defines a square """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# this folder contains the class, methods and object project
+# this folder is written by Mojibola Olalekan
+Happy coding!
