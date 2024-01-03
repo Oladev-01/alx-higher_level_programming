0a9c6b7e49bf0c97bbacf95f7f5bcd93f5178b5b
olalekan <lekanmojibola@gmail.com>
2024-01-03 13:49:56 +0100

    This is a python code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" This module defines a class of square """
+
+
+class Square:
+    """ This class defines a sqaure of variable sizes """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
