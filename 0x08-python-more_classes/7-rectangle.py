9f6e618b1acd64a49b00ec755de940d1e942b742
olalekan <lekanmojibola@gmail.com>
2024-01-09 18:07:39 +0100

    This is a python code

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,12 +6,20 @@ class Rectangle:
     """ This class defines an object of rectangle"""
 
     number_of_instances = 0
-    print_symbol = "#"
-
-    def __init__(self, width=0, height=0):
+    
+    def __init__(self, width=0, height=0, print_symbol="#"):
         self.width = width
         self.height = height
+        self.print_symbol = print_symbol
         Rectangle.number_of_instances += 1
+
+    @property
+    def print_symbol(self):
+        return self.__print_symbol
+    
+    @print_symbol.setter
+    def print_symbol(self, value):
+        self.__print_symbol = value
 
     @property
     def width(self):
@@ -34,32 +42,30 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        if (self.__width == 0 or
-                self.__height == 0):
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (2 * (self.__width + self.__height))
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        if (self.__width == 0 or
-                self.__height == 0):
+        if self.__width == 0 or self.__height == 0:
             return ""
-        sym = str(Rectangle.print_symbol)
+        sym = str(self.print_symbol)
         string = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                string += sym
+                string += str(sym)
             string += "\n"
-        return string.rstrip("\n")
+        return string.rstrip()
 
     def __repr__(self):
-        return f"Rectangle ({self.__width}, {self.__height})"
+        return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
         print("Bye rectangle...")
