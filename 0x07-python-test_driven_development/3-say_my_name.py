893e754ba1eb62bd97b0ae5ba709c9a1a8d1093c
olalekan <lekanmojibola@gmail.com>
2024-01-14 23:20:21 +0100

    This is a python code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This module the string My name is
- <first name> <last name>. the first name and 
- last name represent name of a person and must 
+ <first name> <last name>. the first name and
+ last name represent name of a person and must
  be a string"""
 
 
@@ -11,4 +11,3 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
-
