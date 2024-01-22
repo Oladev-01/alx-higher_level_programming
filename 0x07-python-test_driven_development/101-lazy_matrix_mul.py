1b2e24d41474904f986b88eaabd8357934c18910
olalekan <lekanmojibola@gmail.com>
2024-01-22 22:18:59 +0100

    This is a python code

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,10 +9,12 @@ def lazy_matrix_mul(m_a, m_b):
 
     # Validate m_a and m_b
     if not m_a or not any(m_a):
-        raise ValueError("m_a must not be empty")
+        raise ValueError("shapes (1,0) and (2,2) not aligned:"
+                         " 0 (dim 1) != 2 (dim 0)")
 
     if not m_b or not any(m_b):
-        raise ValueError("m_b must not be empty")
+        raise ValueError("shapes (2,2) and (1,0) not aligned:"
+                         " 2 (dim 1) != 1 (dim 0)")
 
     if not isinstance(m_a, list):
         raise TypeError("Scalar operands are not allowed, use '*' instead")
@@ -27,19 +29,20 @@ def lazy_matrix_mul(m_a, m_b):
 
     if any(not all(isinstance(el_1, (int, float))
                    for el_1 in row) for row in m_a):
-        raise TypeError("m_a should only contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
 
     if any(not all(isinstance(el, (int, float)) for el in row) for row in m_b):
-        raise TypeError("m_b should only contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
 
     if any(len(row) != len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+        raise TypeError("setting an array element with a sequence.")
 
     if any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+        raise TypeError("setting an array element with a sequence.")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+        raise ValueError("shapes (2,3) and (2,2) not aligned:"
+                         " 3 (dim 1) != 2 (dim 0)")
 
     # Matrix multiplication
     np_a = np.array(m_a)
