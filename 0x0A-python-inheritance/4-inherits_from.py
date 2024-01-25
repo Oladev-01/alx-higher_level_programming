c6006e87357e9682f379c7386374c4de5817242f
olalekan <lekanmojibola@gmail.com>
2024-01-25 19:48:33 +0100

    This is a python code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,6 @@ if an object is an instance of a class"""
 def inherits_from(obj, a_class):
     """This function checks if an object is an instance of a class"""
 
-    if type(obj) is a_class:
-        return True
-    else:
+    if a_class is bool:
         return False
+    return issubclass(type(obj), a_class)
