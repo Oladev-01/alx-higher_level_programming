d422287e85a1883272c2378814ee566b1eaccc7b
olalekan <lekanmojibola@gmail.com>
2024-01-26 23:10:22 +0100

    This is a python code

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a class of square that
+ inherits from rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """this is a class of square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/ {self.__size}"
