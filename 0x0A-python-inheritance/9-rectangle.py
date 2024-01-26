57cdd024a541551332c696e4ca8039318704c8d0
olalekan <lekanmojibola@gmail.com>
2024-01-26 22:17:57 +0100

    This is a python code

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a rectangle class"""
+
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
