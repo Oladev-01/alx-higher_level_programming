85cc45b509e8fd10f377e92a7d4a7a7248bed888
olalekan <lekanmojibola@gmail.com>
2024-01-29 17:34:02 +0100

    This is a python code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,9 @@ with encoding UTF-8"""
 def read_file(filename=""):
     """This function opens and read from a file of
     encoding UTF-8"""
+    if not filename:
+        print("")
     with open(filename, 'r', encoding="utf-8") as file:
         opened_file = file.read()
+    opened_file.rstrip("\n")
     print(opened_file)
