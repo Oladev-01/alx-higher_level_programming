ba7b8a676711e0552d32dd2d2eab719098e9d7cf
olalekan <lekanmojibola@gmail.com>
2024-01-29 17:26:56 +0100

    This is a python code

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines a function that reads a text file
+with encoding UTF-8"""
+
+
+def read_file(filename=""):
+    """This function opens and read from a file of
+    encoding UTF-8"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        opened_file = file.read()
+    print(opened_file)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+# This folder is created by Mojibola Olalekan
+# This folder contains the python input and output project
+Happy coding!
