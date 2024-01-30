adfed60007d5a6150e25038669b0742c5f815241
olalekan <lekanmojibola@gmail.com>
2024-01-30 17:20:57 +0100

    This is a python code

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a function that returns the JSON
+representation of a string"""
+import json
+
+
+def to_json_string(my_obj):
+    """This module defines a function that returns the JSON rep
+    of a string"""
+    return json.dump(my_obj)
