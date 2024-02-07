b4625433bc2c00fa037946afdea9d20276ecf522
olalekan <lekanmojibola@gmail.com>
2024-02-07 15:07:02 +0100

    This is a python code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,18 +70,17 @@ class Rectangle(Base):
         for row in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """this method updates the class attributes"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+        keys = ['id', "height", "width", "x", "y"]
+        if args:
+            for key, value in enumerate(args):
+                setattr(self, keys[key], value)
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in keys:
+                    setattr(self, key, value)
 
     def __str__(self):
         string = (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
