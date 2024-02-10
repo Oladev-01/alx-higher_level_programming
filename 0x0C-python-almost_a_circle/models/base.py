fd216050c1b38be7ecb621f13240836953147353
olalekan <lekanmojibola@gmail.com>
2024-02-10 13:53:52 +0100

    This is a python code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,5 +18,5 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or not list_dictionaries:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
