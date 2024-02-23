bb6cc45debc02708664fc9f7353d81c34895694f
olalekan <lekanmojibola@gmail.com>
2024-02-23 16:34:43 +0100

    wrote a SQL query that displays full description of a database from MYSQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- this script prints the full description of first_table
+-- from the database hbtn_0c_0 in MYSQL server
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
