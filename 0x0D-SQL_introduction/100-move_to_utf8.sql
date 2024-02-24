e6d27504084280ad79c48d59a1d8f4f6d9e4a429
olalekan <lekanmojibola@gmail.com>
2024-02-24 01:50:03 +0100

    wrote a sql query that converts hbtn_0c_0 to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- this script converts a database to utf8
+-- Convert hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert first_table to UTF8
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert name field in first_table to UTF8
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
