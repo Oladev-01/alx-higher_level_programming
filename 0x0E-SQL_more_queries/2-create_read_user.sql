87672776e359ae6b5068fadf8ba20be0a9ab7363
olalekan <lekanmojibola@gmail.com>
2024-02-29 02:49:58 +0100

    wrote an mysql query

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2 database to the user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
