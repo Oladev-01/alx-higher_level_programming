d55ee7d685b2f1d34ae27be4dd6cfd1cb125dd62
olalekan <lekanmojibola@gmail.com>
2024-03-01 14:24:30 +0100

    wrote an mysql query

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT g.name AS genre, COUNT(a.show_id) AS number_of_shows
 FROM tv_genres g
 LEFT JOIN tv_show_genres a ON g.id = a.genre_id
-GROUP BY a.name
+GROUP BY g.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
