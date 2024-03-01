ddb66bb4719fc606a60e29277f295a80a0222407
olalekan <lekanmojibola@gmail.com>
2024-03-01 21:01:45 +0100

    wrote an mysql query

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,12 @@
 -- this script displays a record as a result of a query
-SELECT DISTINCT tv_shows.title
+-- Using a subquery to get the genre id for Comedy
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
-WHERE tv_genres.id IS NULL
+LEFT JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+WHERE tv_shows.id NOT IN (
+    SELECT tv_shows.id
+    FROM tv_shows
+    JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY tv_shows.title ASC;
