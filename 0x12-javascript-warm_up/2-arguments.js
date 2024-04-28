f4fb07bd2ebad90c68651c88a6e5f08373348aee
olalekan <lekanmojibola@gmail.com>
2024-04-28 23:58:40 +0100

    Wrote the code for task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+this package checks the number of arguments parsed to
+the file and run statement based on them
+*/
+const argCount = process.argv.length
+
+if (argCount === 2) {
+    console.log("No argument");
+}
+else if (argCount === 3) {
+    console.log("Argument found");
+}
+else {
+    console.log("Arguments found");
+}
