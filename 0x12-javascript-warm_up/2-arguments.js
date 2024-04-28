ec2954e9ffe46f08ec98ff51f372d0cf0d5bb36a
olalekan <lekanmojibola@gmail.com>
2024-04-29 00:02:09 +0100

    fixed linter issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,14 +3,12 @@
 this package checks the number of arguments parsed to
 the file and run statement based on them
 */
-const argCount = process.argv.length
+const argCount = process.argv.length;
 
 if (argCount === 2) {
-    console.log("No argument");
-}
-else if (argCount === 3) {
-    console.log("Argument found");
-}
-else {
-    console.log("Arguments found");
+  console.log('No argument');
+} else if (argCount === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
