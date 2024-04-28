e6f7e3ee1fe4cd6a48dd84aed0f40bfdb8b26cc6
olalekan <lekanmojibola@gmail.com>
2024-04-28 09:14:03 +0100

    fixed an issue as suggested by the linter

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -3,5 +3,5 @@
 this package prints a specific text of string to standard output
 */
 
-const myVar = 'JavaScript is amazing'
-console.log(myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
