1954acc999e47d54214d8c58057fc47c91395559
olalekan <lekanmojibola@gmail.com>
2024-04-28 23:36:18 +0100

    more change on task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,9 +2,6 @@
 /*
 this package prints text of strings to standard output
 */
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'Javascript is amazing';
-console.log(first);
-console.log(second);
-console.log(third);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
