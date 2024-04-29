ef8b529e53fc544e257a06d9606ea7738d2c0146
olalekan <lekanmojibola@gmail.com>
2024-04-29 22:59:00 +0100

    fixed linter issue on task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,24 +5,24 @@ of integers parsed as arguments to the file */
 const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
-  return;
 }
+else {
+    let secLargest = parseInt(args[3]);
+    let larGest = parseInt(args[2]);
 
-let secLargest = parseInt(args[3]);
-let larGest = parseInt(args[2]);
-
-if (secLargest > larGest) {
-  const tmp = larGest;
-  larGest = secLargest;
-  secLargest = tmp;
+    if (secLargest > larGest) {
+        const tmp = larGest;
+        larGest = secLargest;
+        secLargest = tmp;
+    }
+    for (let i = 4; i < args.length; i++) {
+        const num = parseInt(args[i]);
+        if (num > larGest) {
+            secLargest = larGest;
+            larGest = num;
+        } else if (num > secLargest) {
+            secLargest = num;
+        }
+    }
+    console.log(secLargest);
 }
-for (let i = 4; i < args.length; i++) {
-  const num = parseInt(args[i]);
-  if (num > larGest) {
-    secLargest = larGest;
-    larGest = num;
-  } else if (num > secLargest) {
-    secLargest = num;
-  }
-}
-console.log(secLargest);
