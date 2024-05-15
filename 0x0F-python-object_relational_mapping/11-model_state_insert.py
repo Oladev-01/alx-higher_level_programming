65da4fcb32d15e2da310437a5fe8b16f93c911ce
olalekan <lekanmojibola@gmail.com>
2024-05-15 18:01:38 +0100

    query the database via python script connected via an ORM

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""this module queries the database"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def list_states(username, password, database):
+    """this module queries the database"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name='Louisiana'))
+    session.commit()
+
+    state = session.query(State).filter(State.name.like('%Louisiana%')).first()
+    print(state.id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
