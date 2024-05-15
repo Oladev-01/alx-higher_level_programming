eb430048e25ad729cea7e1079eb802121fe80b5e
olalekan <lekanmojibola@gmail.com>
2024-05-15 23:43:43 +0100

    query the database via python script connected via ORM

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -3,8 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
 
 USE hbtn_0e_100_usa;
 
-SELECT cities.id, cities.name FROM states;
+SELECT * FROM states;
 
 SELECT * FROM cities;
-
-SELECT cities.id, states.name, cities.name FROM cities JOIN states where cities.id=states.id
