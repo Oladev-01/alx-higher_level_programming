9711beb1f9428daec111475dbf1294dc71248ac7
olalekan <lekanmojibola@gmail.com>
2024-05-15 11:39:36 +0100

    query the database via python script connected via an ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""this module defines a function isues a query to the database via user input"""
+"""this module defines a function isues a query to the
+ database via user input"""
 
 import sys
 import MySQLdb
+
 
 def list_states(username, password, database, query_type):
     """this function isues a query to the database via user input"""
@@ -19,9 +21,10 @@ def list_states(username, password, database, query_type):
     states = cursor.fetchall()
     for state in states:
         print(state)
-    
+
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
