ba262ef99ae6aae36e13b5c1026dbc9260edfcb0
olalekan <lekanmojibola@gmail.com>
2024-05-15 19:50:37 +0100

    query the database via python script connected via an ORM

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this module queries the database"""
+
+from model_city import Base, City
+from model_state import State
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def list_states(username, password, database):
+    """this module queries the database"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in states:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module defines a class repr of a table in the database"""
+
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
+from model_state import Base
+
+
+class City(Base):
+    """this class defines class repr of the cities table"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format('root', '1002', 'localhost', 'hbtn_0e_14_usa'))
+    Base.metadata.create_all(engine)
