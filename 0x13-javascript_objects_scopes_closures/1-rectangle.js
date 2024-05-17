60844a450b4943eb63b5727ed31b0d5a39874e96
olalekan <lekanmojibola@gmail.com>
2024-05-18 00:43:33 +0100

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// this script defines a class with a constructor instantiating -
+// instance of the class
+
+class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
