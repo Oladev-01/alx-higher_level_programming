92bdb124a172a4610552d2c1a9f7317ed9859389
olalekan <lekanmojibola@gmail.com>
2024-05-19 08:19:32 +0100

    task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,6 @@
  and computes a dictionary of user ids by occurrence. */
 
 const dict = require('./101-data.js').dict;
-console.log(dict);
 const newDict = {};
 
 for (const userId in dict) {
@@ -13,4 +12,5 @@ for (const userId in dict) {
   }
   newDict[occurrence].push(userId);
 }
+
 console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,19 +5,18 @@
 
 const fs = require('fs');
 
-function concatFiles(sourceFile1, sourceFile2, destinationFile) {
-    try {
-        const content1 = fs.readFileSync(sourceFile1, 'utf-8');
-        const content2 = fs.readFileSync(sourceFile2, 'utf-8');
-        const concatenatedFile = content1 + content2;
-        fs.writeFileSync(destinationFile, concatenatedFile, 'utf-8');
-    }
-    catch (error) {
-        console.error('this is the error:', error.message);
-    }
+function concatFiles (sourceFile1, sourceFile2, destinationFile) {
+  try {
+    const content1 = fs.readFileSync(sourceFile1, 'utf-8');
+    const content2 = fs.readFileSync(sourceFile2, 'utf-8');
+    const concatenatedFile = content1 + content2;
+    fs.writeFileSync(destinationFile, concatenatedFile, 'utf-8');
+  } catch (error) {
+    console.error('this is the error:', error.message);
+  }
 }
 
 const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
 if (sourceFile1 && sourceFile2) {
-    concatFiles(sourceFile1, sourceFile2, destinationFile);
+  concatFiles(sourceFile1, sourceFile2, destinationFile);
 }
