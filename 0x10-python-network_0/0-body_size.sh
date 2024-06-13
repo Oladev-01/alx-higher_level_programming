85fa23da0f12c65753986f194f9b3a83d21b2c9c
olalekan <lekanmojibola@gmail.com>
2024-06-13 21:44:26 +0100

    script that returns the size of the response body returned by the request on url passed as argumnet to the script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # this script runs curl on a given url and returns the size of the response body
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-fi
-
-url="$1"
-size=$(curl -s -o /dev/null -w '%{size_download}' "$url")
-echo "$size"
+echo $(curl -s -o /dev/null -w '%{size_download}' "$1")
