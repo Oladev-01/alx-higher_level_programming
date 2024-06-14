322b0ea95ca45574097000c2ddaae81cee09803b
olalekan <lekanmojibola@gmail.com>
2024-06-14 03:31:30 +0100

    A script prompting a url to return all its allowed HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script prompt the ul to return all the acceptable HTTP methods it allows
-echo $(curl -s -X OPTIONS "$1")
+echo $(curl -s -i -X OPTIONS "$1" | grep "Allow:" | sed 's/Allow: //')
