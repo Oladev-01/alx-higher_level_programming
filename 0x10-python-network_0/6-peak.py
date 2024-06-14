5cb0111931449f0c558ab863be1021bb10f95768
olalekan <lekanmojibola@gmail.com>
2024-06-14 16:42:34 +0100

    algorithm to find the peak value in a list of integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,13 +13,9 @@ def find_peak(list_of_integers):
         return arr[0]
     left, right = 0, n - 1
     while left < right:
-        print(f"this is left = {left} and this is right = {right}")
         mid = left + (right - left) // 2
-        print(f"this is mid = {mid}")
         if arr[mid] < arr[mid + 1]:
             left = mid + 1
-            print(f"this is left = {left} and this is right = {right}")
         else:
             right = mid
-            print(f"this is left = {left} and this is right = {right}")
     return arr[left]
