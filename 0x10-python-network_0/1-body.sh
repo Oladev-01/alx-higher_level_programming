906e931c906387990820307f3b9e32e2385b28d6
olalekan <lekanmojibola@gmail.com>
2024-06-14 02:49:18 +0100

    few change

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script sends a GET request to a given URL and prints the body of a 200 status code
-[ "$(curl -s -o /dev/stderr -w '%{http_code}' "$1")" -eq 200 ] && curl -s "$1"
+[ "$(curl -s -L -o /dev/null -w '%{http_code}' "$1")" -eq 200 ] && curl -s -L "$1"
