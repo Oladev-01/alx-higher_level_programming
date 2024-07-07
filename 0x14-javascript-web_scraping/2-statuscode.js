29c316e37f7bcb19e79da39f2c330531f52dd8ca
olalekan <lekanmojibola@gmail.com>
2024-07-07 10:49:17 +0100

    getting the status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,12 +3,13 @@
  * this package displays the status code of a GET request
  */
 
-const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
+const request = require('request');
+const urlPath = process.argv[2];
 
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-    }
+// using the request module to query the url
+request(urlPath, (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+  }
+  console.log('code:', response.statusCode);
 });
