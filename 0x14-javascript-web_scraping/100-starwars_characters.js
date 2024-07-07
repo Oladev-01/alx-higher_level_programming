a53476b14c9c274e60bdd9b7c14082c568f60a20
olalekan <lekanmojibola@gmail.com>
2024-07-07 15:18:32 +0100

    characters in a movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/**
+ * this script prints all the characters in a star war
+ * movie. episode is specified by the Id
+ */
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  const film = JSON.parse(body);
+  film.characters.forEach(character => {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.error(err);
+      }
+      const nameCharacter = JSON.parse(body);
+      console.log(nameCharacter.name);
+    });
+  });
+});
