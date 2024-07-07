5ed38c9487eb839de4d1e643cda8250e488e4193
olalekan <lekanmojibola@gmail.com>
2024-07-07 13:54:23 +0100

    requesting from a webpage and writing to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,21 +7,21 @@
 const request = require('request');
 const fs = require('fs');
 if (process.argv.length !== 4) {
-    console.error('Usage: 5-request_store.js <url> <filename>');
-    process.exit(1);
+  console.error('Usage: 5-request_store.js <url> <filename>');
+  process.exit(1);
 }
 const url = process.argv[2];
 const fileToWrite = process.argv[3];
 
 request(url, (err, response, body) => {
-    if (err) {
-        console.error(err);
-    }
-    if (response.statusCode === 200) {
-        fs.writeFile(fileToWrite, body, (err) => {
-            if (err) {
-                console.error('Error:', err);
-            }
-        });
-    }
+  if (err) {
+    console.error(err);
+  }
+  if (response.statusCode === 200) {
+    fs.writeFile(fileToWrite, body, (err) => {
+      if (err) {
+        console.error('Error:', err);
+      }
+    });
+  }
 });
